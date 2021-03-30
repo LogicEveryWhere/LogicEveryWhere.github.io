c439e3a742307e34075403966f4f22f72e1984c2
LogicEveryWhere <mr.preston.j@gmail.com>
2021-03-30 22:53:32 +0300

    minor update

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,8 +57,8 @@ export default function Home() {
     const {siteConfig = {}} = context;
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />">
+            title={`${siteConfig.title}`}
+            description="База знань ЖК Great, телеграм ЖК Great, форум ЖК Great">
             <header className={clsx('hero hero--primary', styles.heroBanner)}>
                 <div className="container">
                     <h1 className="hero__title">{siteConfig.title}</h1>
